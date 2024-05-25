08a0b8e6e3f6c98a9f69e47d12333680b70e0183
David_Calvo <david.calvo@mataro.epiaedu.cat>
2024-05-25 19:22:19 +0200

    preguntas añadidas

--- codigo.js
+++ codigo.js
@@ -11,13 +11,10 @@ function opcionCorrectaCine1(){
 };
 
 
-
 function opcionIncorrectaCine1(){
  
     window.location.href = "../correctes/incorrecteCine1.html";
 }
-
-
 
 
 function opcionCorrectaCine2(){
@@ -28,4 +25,204 @@ function opcionCorrectaCine2(){
 
 function opcionIncorrectaCine2(){
     window.location.href = "../correctes/incorrecteCine2.html";
+}
+
+
+
+function opcionCorrectaCine3(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine3.html";
+}
+
+
+function opcionIncorrectaCine3(){
+    window.location.href = "../correctes/incorrecteCine3.html";
+}
+
+
+function opcionCorrectaCine4(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine4.html";
+}
+
+function opcionIncorrectaCine4(){
+    window.location.href = "../correctes/incorrecteCine4.html";
+}
+
+function opcionCorrectaCine5(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine5.html";
+}
+
+function opcionIncorrectaCine5(){
+    window.location.href = "../correctes/incorrecteCine5.html";
+}
+
+// Repeat the above pattern for options 6 to 20
+function opcionCorrectaCine6(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine6.html";
+}
+
+function opcionIncorrectaCine6(){
+    window.location.href = "../correctes/incorrecteCine6.html";
+}
+
+// Continue adding the remaining functions for options 7 to 20
+function opcionCorrectaCine7(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine7.html";
+}
+
+function opcionIncorrectaCine7(){
+    window.location.href = "../correctes/incorrecteCine7.html";
+}
+
+// Add functions for options 8 to 20
+function opcionCorrectaCine8(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine8.html";
+}
+
+function opcionIncorrectaCine8(){
+    window.location.href = "../correctes/incorrecteCine8.html";
+}
+
+// Continue adding the remaining functions for options 9 to 20
+function opcionCorrectaCine9(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine9.html";
+}
+
+function opcionIncorrectaCine9(){
+    window.location.href = "../correctes/incorrecteCine9.html";
+}
+
+// ...
+
+function opcionCorrectaCine10(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine10.html";
+}
+
+function opcionIncorrectaCine10(){
+    window.location.href = "../correctes/incorrecteCine10.html";
+}
+
+function opcionCorrectaCine11(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine11.html";
+}
+
+function opcionIncorrectaCine11(){
+    window.location.href = "../correctes/incorrecteCine11.html";
+}
+
+function opcionCorrectaCine12(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine12.html";
+}
+
+function opcionIncorrectaCine12(){
+    window.location.href = "../correctes/incorrecteCine12.html";
+}
+
+function opcionCorrectaCine13(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine13.html";
+}
+
+function opcionIncorrectaCine13(){
+    window.location.href = "../correctes/incorrecteCine13.html";
+}
+
+function opcionCorrectaCine14(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine14.html";
+}
+
+function opcionIncorrectaCine14(){
+    window.location.href = "../correctes/incorrecteCine14.html";
+}
+
+function opcionCorrectaCine15(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine15.html";
+}
+
+function opcionIncorrectaCine15(){
+    window.location.href = "../correctes/incorrecteCine15.html";
+}
+
+function opcionCorrectaCine16(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine16.html";
+}
+
+function opcionIncorrectaCine16(){
+    window.location.href = "../correctes/incorrecteCine16.html";
+}
+
+function opcionCorrectaCine17(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine17.html";
+}
+
+function opcionIncorrectaCine17(){
+    window.location.href = "../correctes/incorrecteCine17.html";
+}
+
+function opcionCorrectaCine18(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine18.html";
+}
+
+function opcionIncorrectaCine18(){
+    window.location.href = "../correctes/incorrecteCine18.html";
+}
+
+function opcionCorrectaCine19(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine19.html";
+}
+
+function opcionIncorrectaCine19(){
+    window.location.href = "../correctes/incorrecteCine19.html";
+}
+
+
+function opcionCorrectaCine20(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine20.html";
+}
+
+function opcionIncorrectaCine20(){
+    window.location.href = "../correctes/incorrecteCine20.html";
+}
+
+function opcionCorrectaCine20(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine20.html";
+}
+
+function opcionIncorrectaCine20(){
+    window.location.href = "../correctes/incorrecteCine20.html";
+}
+
+function opcionCorrectaCine20(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine20.html";
+}
+
+function opcionIncorrectaCine20(){
+    window.location.href = "../correctes/incorrecteCine20.html";
+}
+
+function opcionCorrectaCine20(){
+    puntosCine++;
+    window.location.href = "../correctes/correcteCine20.html";
+}
+
+function opcionIncorrectaCine20(){
+    window.location.href = "../correctes/incorrecteCine20.html";
 }
